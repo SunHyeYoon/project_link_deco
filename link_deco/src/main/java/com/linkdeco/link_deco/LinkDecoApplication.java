bd22cb7d4726bd07ced7fa91abb68d01bb52500b
SunHyeYoon <haehae96@jbnu.ac.kr>
2026-01-13 20:55:36 +0900

    Feat: 프로젝트 기본 세팅

---
+++ link_deco/src/main/java/com/linkdeco/link_deco/LinkDecoApplication.java
@@ -0,0 +1,13 @@
+package com.linkdeco.link_deco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkDecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkDecoApplication.class, args);
+	}
+
+}
---
+++ link_deco/src/test/java/com/linkdeco/link_deco/LinkDecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkdeco.link_deco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkDecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

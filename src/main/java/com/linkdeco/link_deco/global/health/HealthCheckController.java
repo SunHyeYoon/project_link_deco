fd5240747a97dc5d86df87a381257108af0b428e
SunHyeYoon <haehae96@jbnu.ac.kr>
2026-01-21 18:52:13 +0900

    feat: health check controller 추가

---
+++ src/main/java/com/linkdeco/link_deco/global/health/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.linkdeco.link_deco.global.health;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+}
